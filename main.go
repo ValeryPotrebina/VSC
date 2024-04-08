87f52e2c2fbe9ab83f7d87bb9216210621a90ae1
ValeryPotrebina <valery_potrebina_2003@mail.ru>
2024-04-08 18:29:58 +0300

    added

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-package blockchain
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("first")
+	chain.AddBlock("second")
+	chain.AddBlock("third")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Prev hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println("-------------------------")
+	}
+}
